ef9e681a0330fbed0a8fa4cc9b2703a0e40e2107
anaximeno <anaximenobrito@gmail.com>
2020-12-20 19:09:58 -0100

    unnecessary code removed and code improved

--- usr/lib/shazam/shazam.py
+++ usr/lib/shazam/shazam.py
@@ -2,10 +2,17 @@
 # TODO: must check if the imports of all modules are working well, if not, mssss
 
 import sys
+import hashlib as hlib
 import argparse
 
-from common import Process, CheckVars, out_error, hlist
+from common import Process, CheckVars, out_error
 
+hlist = {
+	"type": ["md5", "sha1", "sha224", "sha256", "sha384", "sha512"],
+	"hash": [hlib.md5(), hlib.sha1(), hlib.sha224(), hlib.sha256(), hlib.sha384(), hlib.sha512()],
+	"othernames": ["md5sum", "sha1sum", "sha224sum", "sha256sum", "sha384sum", "sha512sum"],
+	"pluralnames": ["md5sums", "sha1sums", "sha224sums", "sha256sums", "sha384sums", "sha512sums"]
+}
 
 parser = argparse.ArgumentParser(
 	description="Check and Compare the sums.",
@@ -24,22 +31,23 @@ option.add_argument("-F", "--Files",
 option.add_argument("-A", "--all", help="Print all the file's sums.")
 
 option.add_argument(
-    "-v", "--version", help="Print the current version of this app.", action="store_true")
+	"-v", "--version", help="Print the current version of this app.", action="store_true")
 
 option.add_argument(
-    "content", help="file name or sum depending of the choice", nargs='?', default=None)
+	"content", help="file name or sum depending of the choice", nargs='?', default=None)
 
 parser.add_argument("--verbose", help="Verbose response", action="store_true")
 
 for item in hlist['type']:
 	option.add_argument("-%s" % item, help="to compare the file's hash",
-	                    metavar='', nargs=2)  # metavar is empty
+						metavar='', nargs=2)  # metavar is empty
 	option.add_argument("--%ssum" % item,
 						metavar='', help="to get the file's hash")
 
 
-# TODO: Ainda falta fazer o processamento na classe para versão e verbose e none opt
-
+# TODO: Ainda falta fazer o processamento na classe para verbose
+# TODO: Quando for do tipo ler arquivo de txt e fazer para um só arquivo, 
+# precisa tentar todo os nomes dentro e procurar o que funciona
 class MainFlow:
 
 	def __init__(self, args):
@@ -78,7 +86,7 @@ class MainFlow:
 					self.gsum = None
 
 			if args.file:
-				checkv = CheckVars(filename=args.file, givensum=None)
+				checkv = CheckVars(filename=args.file, givensum=None, hashlist=hlist)
 				found, unfounded = checkv.analyze_text()
 				if found:
 					self.fname, self.gsum, self.stype = found[0]
@@ -108,94 +116,15 @@ class MainFlow:
 			self.process.multifiles()
 		elif self.args.all or self.args.content:
 			self.process.allsums()
-
-
-if __name__ == '__main__':
-	mf = MainFlow(parser.parse_args())
-	mf.make_process()
-
-'''	*Superposition*
-def main():
-
-    def make_process(givensum, sumtype, filename):
-        procs = Process(filename=filename, sumtype=sumtype, givensum=givensum)
-
-        if givensum:
-            procs.normal()
-
-            if args.verbose:
-                procs.verbose()
-
-        else:
-            print("It will only show the file's %s!" % sumtype)
-            procs.only_sum()
-
-    if args.version:
-        with open("/usr/share/shazam/VERSION", "rt") as f:
-            print("ShaZam: %s" % str(f.read()))
-    elif args.file:
-        if args.content:
-            checks = CheckVars(filename=args.content, givensum=None)
-            found, unfounded = checks.analyze_text()
-            if found:
-                fname, gsum, stype = found[0]
-                procs = Process(filename=fname, sumtype=stype, givensum=gsum)
-                procs.normal()
-            else:
-                out_error("None of these file(s) was/were found:", exit=False)
-                for unf in unfounded:
-                    print("*", unf)
-                sys.exit(1)
-        else:
-            parser.error("expected one argument")
-    elif args.Files:
-        if args.content:
-            checks = CheckVars(filename=args.content, givensum=None)
-            found, unfounded = checks.analyze_text()
-            if found:
-                for i in range(len(found)):
-                    fname, gsum, stype = found[i]
-                    procs = Process(filename=fname, givensum=gsum, sumtype=stype)
-                    procs.normal()
-                if unfounded:
-                    print("The file(s) below was/were not found:")
-                    for unf in unfounded:
-                        print("* ", unf)
-            else:
-                print("None of the file(s) below was/were found:")
-                for unf in unfounded:
-                    print("* ", unf)
-        else:
-            parser.error("expected one argument")
-    elif args.all:
-        if args.content:
-            procs = Process(args.content)
-            procs.allsums()
-        else:
-            out_error("expected one argument")
-    elif args.md5sum:
-        make_process(args.content, "md5", args.md5sum)
-    elif args.sha1sum:
-        make_process(args.content, "sha1", args.sha1sum)
-    elif args.sha224sum:
-        make_process(args.content, "sha224", args.sha224sum)
-    elif args.sha256sum:
-        make_process(args.content, "sha256", args.sha256sum)
-    elif args.sha384sum:
-        make_process(args.content, "sha384", args.sha384sum)
-    elif args.sha512sum:
-        make_process(args.content, "sha512", args.sha512sum)
-    elif args.content:  # must be the penultimate
-        want_all = input(f"Do you want to check all '{args.content}' sums? [Y/n]: ")
-        if not want_all or want_all.isspace() or want_all.lower() == "yes" or want_all.lower() == "y":
-            print("")  # skip one line
-            procs = Process(args.content)
-            procs.allsums()
-        else:
-            out_error("Aborted!\nusage: shazam [OPTION] content...")
-    else:
-        out_error("No options was chosen, \ntry: shazam -h, for more informations.")
+		elif self.args.version:
+			with open("/usr/share/shazam/VERSION", "rt") as ver:
+				print("ShaZam", str(ver.read()))
 
 if __name__ == '__main__':
-    main()
-'''
+	if len(sys.argv) > 1:
+		mf = MainFlow(parser.parse_args())
+		mf.make_process()
+	else:
+		print("Usage: shazam [Option] ARGUMENTS..")
+		print("       shazam --help			display the help section and exit")
+		print("       shazam --version		display the Version information and exit")
