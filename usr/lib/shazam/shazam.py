5370d3a54922c29d98841cde9d197d5ced12cff0
anaximeno <anaximenobrito@gmail.com>
2021-01-03 18:19:09 -0100

    code reorganizated to receive OOP classes from common.py

--- usr/lib/shazam/shazam.py
+++ usr/lib/shazam/shazam.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
 import sys
-import hashlib as hlib
 from time import sleep
 
-from common import Process, CheckVars, out_error, _hex, results
+from common import Process, Analyze, FileId, Out
 
 try:
 	import argparse
 	from alive_progress import alive_bar
 except ImportError:
-	out_error("Important Modules are not installed yet: argparse, alive_progress\n\
-		Install them with: pip/pip3 install argparse alive_progress")
+	Out.error("Important Modules are not installed yet: argparse, alive_progress",
+			  "\nInstall them with: pip/pip3 install argparse alive_progress")
 
-hlist = {
-	"md5": hlib.md5(),
-	"sha1": hlib.sha1(),
-	"sha224": hlib.sha224(),
-	"sha256": hlib.sha256(),
-	"sha384": hlib.sha384(),
-	"sha512": hlib.sha512()
-}
+if len(sys.argv) == 2:
+	cont = True
+else:
+	cont = False
 
-
-# TODO: Ainda falta fazer o processo para verbose
-class MainFlow(object):
+# TODO: coloca only_sum pa calcula mas de um arquivos (elimina args.contente, e usa sys.argv pa get file o que ca escodjedo opts)
+class MainFlow:
 
 	def __init__(self, args):
 		self.args = args
+		self.process = Process()
 
-		self.stype = None
-		self.fname = None
-		self.gsum = None
-
-		options = {
+		self.options = {
 			'md5': (args.md5, args.md5sum),
 			'sha1': (args.sha1, args.sha1sum),
 			'sha224': (args.sha224, args.sha224sum),
@@ -41,99 +32,77 @@ class MainFlow(object):
 			'sha512': (args.sha512, args.sha512sum)
 		}
 
-		if args.file:
-			checkv = CheckVars(filename=args.file, givensum=None)
-			found, unfound = checkv.analyze_text(hlist)
-
-			found = list(found)
-
-			if found:
-				self.fname, self.gsum, self.stype = found[0]
-			else:
-				print("This/these file(s) wasn't/weren't found:")
-				for f in unfound:
-					print("* ", f)
-				sys.exit(0)
-
-		elif args.Files:
-			self.fname = args.Files
-
-		elif args.all:
-			self.fname = args.all
-
-		elif args.content:
-			self.fname = args.content
-
-		else:
-			for stype, opt in options.items():
-				if opt[0]:
-					# TODO: pass the expression below to a function, to be used when reading txt files
-					if _hex(opt[0][1]):
-						self.fname, self.gsum = opt[0]
-					elif _hex(opt[0][0]):
-						self.gsum, self.fname = opt[0]			
-					else:
-						out_error(f"File sum not hexadecimal value: '{opt[0][0]} {opt[0][1]}'")
-					self.stype = stype
-					break
-				elif opt[1]:
-					self.fname = opt[1]
-					self.stype = stype
-					self.gsum = None
-					break
-		
-		if self.fname:
-			self.process = Process(filename=self.fname, sumtype=self.stype, givensum=self.gsum)
-		
-	def make_process(self):  # TODO: args.file: must find search for all the files until find one Sucess case
-		if self.fname and self.stype and self.gsum or self.args.file:
-			self.process.make_and_check()
-
-		elif self.fname and self.stype:
-			self.process.only_show_sum()
-
-		elif self.args.Files:
-			checkv = CheckVars(self.fname, givensum=None)
-			found, unfound = checkv.analyze_text(hlist)
-
-			def check_multifiles(filename, givensum, sumtype):
-				p = Process(
-					filename=filename,
-					sumtype=sumtype,
-					givensum=givensum,
-				)
-
-				p.make.read(sumtype, p.make.gen_data(bars=False))
-				if int(p.hlist[sumtype].hexdigest(), 16) == int(givensum, 16):
-					return True
-				else:
-					return False
-
-			nfound = list(found)
-			if nfound:
-				with alive_bar(len(nfound), bar='blocks', spinner='dots') as bar:
-					for fname, gsum, stype in nfound:
-						results(fname, stype, check_multifiles(fname, gsum, stype))
-						# when lower is the sleep value, faster will be the reading,
-						# but it will increase the CPU usage
-						sleep(0.00001)
-						bar()
-
-			print("\nThis/these file(s) wasn't/weren't found:")
-			for f in unfound:
-				print("* ", f)
-			print('\n ** If nothing appears above, it means that all files have been found **')
-
-		elif self.args.all or self.args.content:
-			self.process.show_allsums()
-
-		elif self.args.version:
+	def make_process(self):
+		if self.args.version:
 			with open("/usr/share/shazam/VERSION", "rt") as ver:
 				print("ShaZam", str(ver.read()).strip())
+			sys.exit()
+		elif self.args.file:
+			stype = Analyze.sumtype(self.args.file)
+			content = Analyze.analyze_text(self.args.file)
 
+			for fname, gsum in content.items():
+				f = FileId(fname, gsum)
+				if f.existence is True:
+					self.process.addFile(f)
+					break
+			else:
+				print("\nThis/these file(s) wasn't/weren't found:")
+				for filename in content.keys():
+					print("* ", filename)
+				sys.exit()
+			
+			self.process.define_sumtype(stype)
+			self.process.normal_process()
+		elif self.args.Files:
+			stype = Analyze.sumtype(self.args.Files)
+			content = Analyze.text_content(self.args.Files)
+			
+			for fname, gsum in content.items():
+				f = FileId(fname, gsum)
+				self.process.addFile(f)
+
+			self.process.define_sumtype(stype)
+			self.process.check_multifiles()
+		elif self.args.all or cont:
+			if self.args.all:
+				fname = self.args.all
+			else:
+				fname = self.args.content
+			f = FileId(fname)
+			self.process.addFile(f)
+			self.process.show_allsums()
 		else:
-			out_error("Anything went wrong")
-
+			for stype, option in self.options.items():
+				norm, only = option
+				if norm:
+					if Analyze.is_hex(norm[0]):
+						gsum, fname = norm[0], norm[1]
+					elif Analyze.is_hex(norm[1]):
+						fname, gsum = norm[0], norm[1]
+					else:
+						Out.error(f"Given sum not recognized: {norm[0], norm[1]}")
+					f = FileId(fname, gsum)
+					self.process.addFile(f)
+					self.process.define_sumtype(stype)
+					self.process.normal_process()
+					break
+				elif only:
+					if type(only) is list:
+						for item in only:
+							f = FileId(item)
+							self.process.addFile(f)
+							self.process.define_sumtype(stype)
+							self.process.only_show_sum()
+							break
+					else:
+						f = FileId(only)
+						self.process.addFile(f)
+						self.process.define_sumtype(stype)
+						self.process.only_show_sum()
+						break
+			else:
+				Out.error('No option was chosen!')
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
@@ -150,14 +119,13 @@ if __name__ == '__main__':
 	option.add_argument("-a", "--all", help="Print all the file's sums.", metavar='')
 
 	option.add_argument("-v", "--version", help="Print the current version of this program.", action="store_true")
+	
+	if cont:
+		option.add_argument("content", help="file name or sum depending of option the choiced", nargs='?')
 
-	option.add_argument("content", help="file name or sum depending of the choice", nargs='?', default=None)
-
-	# parser.add_argument("--verbose", help="Verbose response", action="store_true")
-
-	for item in hlist.keys():
+	for item in Analyze.sumtypes_list:
 		option.add_argument("-%s" % item, help="for comparing the file's hash", metavar='', nargs=2)
-		option.add_argument("--%ssum" % item, metavar='', help="for just getting the file's hash")
+		option.add_argument("--%ssum" % item, metavar='', help="for just getting the file's hash", nargs='?')
 
 	if len(sys.argv) > 1:
 		mf = MainFlow(parser.parse_args())
