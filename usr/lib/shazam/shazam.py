20b80320ffc15e217fd4b1612fb820670f6ebc07
anaximeno <anaximenobrito@gmail.com>
2021-02-08 18:40:21 -0100

    Update to V=2021.2 now it receives command of the bash file

--- usr/lib/shazam/shazam.py
+++ usr/lib/shazam/shazam.py
@@ -13,8 +13,17 @@ Prerequesites:
 
 """
 
+
+try :
+	with open("/usr/share/shazam/VERSION", "rt") as ver :
+		vers = str(ver.read()).strip()
+
+except FileNotFoundError :
+	vers = 'Undefined'
+
+
 __author__ = "Anaxímeno Brito"
-__current_version__ = "2021.1"
+__version__ = vers
 __license__ = "GNU General Public License v3.0"
 __copyright__ = "Copyright (c) 2021 by Anaxímeno Brito"
 
@@ -22,159 +31,80 @@ __copyright__ = "Copyright (c) 2021 by Anaxímeno Brito"
 import sys
 from common import Process, Analyze, FileId, Out
 
-argv_len = len(sys.argv)
 
-try:
+try :
 	import argparse
 
-except ImportError:
+except ImportError :
 	Out.error("Important Module is not installed yet: argparse",
 			  "\nInstall it with: pip/pip3 install argparse")
 
 
-# TODO: coloca only_sum pa calcula mas de um arquivos 
-# (elimina args.content, e usa sys.argv pa get file oh que ca scodjedo opts)
-class MainFlow:
+class MainFlow :
 
-	def __init__(self, args):
+	def __init__(self, args) :
 		self.args = args
 		self.process = Process()
 
-		self.options = {
-			'md5': (args.md5, args.md5sum),
-			'sha1': (args.sha1, args.sha1sum),
-			'sha224': (args.sha224, args.sha224sum),
-			'sha256': (args.sha256, args.sha256sum),
-			'sha384': (args.sha384, args.sha384sum),
-			'sha512': (args.sha512, args.sha512sum)
-		}
-
-	def make_process(self):
-		if self.args.version:
-			try:				
-				with open("/usr/share/shazam/VERSION", "rt") as ver:
-					print("ShaZam", str(ver.read()).strip())
-
-				sys.exit()
-
-			except FileNotFoundError:
-				Out.error("VERSION file was not found")
-
-		elif self.args.file:
-			self.process.define_sumtype(Analyze.sumtype(self.args.file))
-			content = Analyze.contents(self.args.file)
-
-			for fname, gsum in content.items():
-				f = FileId(fname, gsum)
-
-				if f.existence is True:
-					self.process.add_file(f)
-					break
-
-			else:
-				print("\nThis/these file(s) wasn't/weren't found:")
-
-				for filename in content.keys(): 
-					print("* ", filename)
-				
-				sys.exit()
+	def make_process(self) :
+		"""Analysis command args and make specified process"""
+		if self.args.type :
+			self.process.define_sumtype(self.args.type)
 			
-			self.process.check_process()
+			if self.args.content :
+				filename, givensum = self.args.content
 
-		elif self.args.Files:
-			self.process.define_sumtype(Analyze.sumtype(self.args.Files))
-			content = Analyze.contents(self.args.Files)
-			
-			for fname, gsum in content.items():
-				f = FileId(fname, gsum)
-				self.process.add_file(f)
+				self.process.add_file(FileId(filename, givensum))
+				self.process.check_process()
+
+			elif self.args.files :
+				for filename in self.args.files :
+					self.process.add_file(FileId(filename))
+				self.process.only_show_sum()
+
+			else :
+				Out.error("Spected more arguments")
+
+		elif self.args.read :
+			txt_file = self.args.read[0]
+			self.process.define_sumtype(Analyze.sumtype(txt_file))
+
+			for filename, givensum in Analyze.contents(txt_file).items() :
+				self.process.add_file(FileId(filename, givensum))
 
 			self.process.check_multifiles()
 
-		elif self.args.all or argv_len == 2:
-			if self.args.all:
-				fname = self.args.all
-
-			else:
-				fname = self.args.content
-
-			f = FileId(fname)
-			self.process.add_file(f)
+		elif self.args.all :
+			self.process.add_file(FileId(self.args.all[0]))
 			self.process.show_allsums()
-		
-		else:
-			for stype, option in self.options.items():
-				norm, only = option
-				if norm:
-					if Analyze.is_hex(norm[0]):
-						gsum, fname = norm
-		
-					elif Analyze.is_hex(norm[1]):
-						fname, gsum = norm
-		
-					else:
-						if Analyze.exists(norm[0]):
-							Out.error(f"Given sum not recognized: {norm[1]!r}")
-		
-						elif Analyze.exists(norm[1]):
-							Out.error(f"Given sum not recognized: {norm[0]!r}")
-		
-						else:
-							Out.error(f"File given hashsum was not recognized as hexadecimal: {norm[0], norm[1]}")
 
-					f = FileId(fname, gsum)
-					self.process.add_file(f)
-					self.process.define_sumtype(stype)
-					self.process.check_process()
-					break
-		
-				elif only:
-					if type(only) is list:
-						for item in only:
-							f = FileId(item)
-							self.process.add_file(f)
-							self.process.define_sumtype(stype)
-							self.process.only_show_sum()
-							break
+		else :
+			Out.error('No option was chosen!')
+			
 
-					else:
-						f = FileId(only)
-						self.process.add_file(f)
-						self.process.define_sumtype(stype)
-						self.process.only_show_sum()
-						break
-
-			else:
-				Out.error('No option was chosen!')
-
-if __name__ == '__main__':
+if __name__ == '__main__' :
 	parser = argparse.ArgumentParser(
+		prog="ShaZam",
 		description="Checks and Compare the sums.",
 		usage="shazam [OPTION] content..."
 	)
+	parser.add_argument("--version", help="Print the current version of this program.", action='version', version='%(prog)s {}'.format(__version__))
 
-	option = parser.add_mutually_exclusive_group()
+	# TODO: Check the help output
+	parser.add_argument("--type", choices=Analyze.sumtypes_list)
+	parser.add_argument("--content", nargs=2)
+	parser.add_argument("-A", "--all", nargs=1)
+	parser.add_argument("-f", "--files", nargs='+')
+	parser.add_argument("-r", "--read", nargs=1)
 
-	option.add_argument("-f", "--file", metavar='', help="Checks the sum of only one file written in the text file.")
+	args = parser.parse_args()
 
-	option.add_argument("-F", "--Files", metavar='', help="Checks the sum of all files written  in the text file.")
 
-	option.add_argument("-a", "--all", help="Print all the file's sums.", metavar='')
-
-	option.add_argument("-v", "--version", help="Print the current version of this program.", action="store_true")
-
-	if argv_len == 2:
-		option.add_argument("content", help="file name or sum depending of option the choiced", nargs='?')
-
-	for item in Analyze.sumtypes_list:
-		option.add_argument("-%s" % item, help="for comparing the file's hash", metavar='', nargs=2)
-		option.add_argument("--%ssum" % item, metavar='', help="for just getting the file's hash", nargs='?')
-
-	if argv_len > 1:
-		mf = MainFlow(parser.parse_args())
+	if len(sys.argv) > 1 :
+		mf = MainFlow(args)
 		mf.make_process()
 
-	else:
+	else :
 		print("Usage: shazam [Option] ARGUMENTS..")
 		print("       shazam --help         display the help section and exit")
 		print("       shazam --version      display the Version information and exit")
